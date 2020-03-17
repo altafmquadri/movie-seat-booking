07b6bcbcb08a5b0347dfc4cb714aeed270857e64
Altaf Quadri <altafmquadri@gmail.com>
2020-03-17 16:14:14 -0400

    localstorage retrieved, project done

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
 
+populateUI()
+
 let ticketPrice = +movieSelect.value
 
 //Save selected movie index and price
@@ -26,6 +28,23 @@ const updateSelectedCount = () => {
     window.localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
 }
 
+//Get data from localstorage and populate UI
+
+function populateUI() {
+    const selectedSeats = JSON.parse(window.localStorage.getItem('selectedSeats'))
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected')
+            }
+        })
+    }
+    const selectedMovieIndex = window.localStorage.getItem('selectedMovieIndex')
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+}
+
 //Movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value
@@ -40,4 +59,7 @@ container.addEventListener('click', e => {
         updateSelectedCount()
     }
 })
+
+//Initial count and total set
+updateSelectedCount()
 
